cea45342ddd01ddb057a6f8a98d35f55a01864a6
Blaze Horizon <blazenewhorizon@gmail.com>
2025-08-01 14:48:08 -0400

    Add camera zoom controls to level1 scene
    Introduced zoom in/out functionality to Camera2D using mouse scroll actions, with configurable zoom limits. Updated level1.tscn to set up zoom parameters and moved Camera2D under CanvasLayer for improved scene organization.

--- scenes/maps/camera_2d.gd
+++ scenes/maps/camera_2d.gd
@@ -1,8 +1,10 @@
 extends Camera2D
 
 @export var panning_speed: int
-
+@export var zoom_max: Vector2
+@export var zoom_min: Vector2
 var direction: Vector2
+
 
 
 func _process(delta: float) -> void:
@@ -12,3 +14,14 @@ func _process(delta: float) -> void:
 	
 	if direction != Vector2.ZERO:
 		position += direction * panning_speed * delta
+		
+# Code to zoom camera in and out during gameplay. 
+	if Input.is_action_just_pressed("prelaunch_scroll_down") and zoom > zoom_min:
+		zoom -= Vector2(0.20, 0.20)
+	if Input.is_action_just_pressed("prelaunch_scroll_up") and zoom < zoom_max:
+		zoom += Vector2(0.20, 0.20)
+
+## The car has it's own camera built in, maybe best to queue_free this camera 
+## so that one takes over after launch? Something like this:
+#func _on_launch_pressed() -> void:
+#	queue_free()
